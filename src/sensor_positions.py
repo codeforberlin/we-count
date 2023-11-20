63b611011d079e11c722edc465c38928dfba3fff
Michael Behrisch <oss@behrisch.de>
2023-11-20 08:28:41 +0100

    making position retrieval configurable

--- src/sensor_positions.py
+++ src/sensor_positions.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright (c) 2023 Michael Behrisch
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -21,9 +21,31 @@
 # @author  Michael Behrisch
 # @date    2023-01-03
 
+import os
 import http.client
 import json
 import time
+import datetime
+import argparse
+
+
+def get_options(args=None):
+    base = os.path.abspath(os.path.dirname(__file__))
+    parser = argparse.ArgumentParser()
+    # Berlin as in https://github.com/DLR-TS/sumo-berlin
+    parser.add_argument("-b", "--bbox", default="12.78509,52.17841,13.84308,52.82727",
+                        help="bounding box to retrieve in geo coordinates west,south,east,north")
+    parser.add_argument("-u", "--url", default="telraam-api.net",
+                        help="Download from the given Telraam server")
+    parser.add_argument("-t", "--token-file", default=os.path.join(base, "telraam-token.txt"),
+                        metavar="FILE", help="Read Telraam API token from FILE")
+    parser.add_argument("-j", "--json-file", default=os.path.join(base, "..", "sensor-geojson.js"),
+                        metavar="FILE", help="Write Geo-JSON output to FILE")
+    parser.add_argument("--camera", action="store_true", default=False,
+                        help="include individual cameras")
+    parser.add_argument("-v", "--verbose", action="store_true", default=False,
+                        help="enable verbose output")
+    return parser.parse_args(args=args)
 
 
 def add_camera(conn, headers, res):
@@ -37,19 +59,24 @@ def add_camera(conn, headers, res):
 
 
 def main():
-    bbox = "12.78509,52.17841,13.84308,52.82727"  # Berlin as in https://github.com/DLR-TS/sumo-berlin
-    conn = http.client.HTTPSConnection("telraam-api.net")
-    with open('telraam-token.txt') as token:
-        headers = { 'X-Api-Key': token.read() }
-    # conn.request("GET", "/v1/cameras", '', headers)
-    # conn.request("GET", "/v1/segments/id/348917", '', headers)
-    # conn.request("GET", "/v1/reports/traffic_snapshot_live", '', headers)
-    payload = '{"time":"live", "contents":"minimal", "area":"%s"}' % bbox
+    options = get_options()
+    if os.path.exists(options.json_file):
+        last_mod = datetime.datetime.fromtimestamp(os.path.getmtime(options.json_file))
+        delta = datetime.timedelta(hours=1)
+        if datetime.datetime.now() - last_mod < delta:
+            if options.verbose:
+                print(f"Not recreating {options.json_file}, it is less than {delta} old.")
+            return
+    conn = http.client.HTTPSConnection(options.url)
+    with open(options.token_file, encoding="utf8") as token:
+        headers = { 'X-Api-Key': token.read().strip() }
+    payload = '{"time":"live", "contents":"minimal", "area":"%s"}' % options.bbox
     conn.request("POST", "/v1/reports/traffic_snapshot", payload, headers)
     res = json.loads(conn.getresponse().read())
-    with open("sensor-geojson.js", "w") as sensor_js:
+    with open(options.json_file, "w", encoding="utf8") as sensor_js:
         print("var sensors = ", file=sensor_js, end='')
-        # add_camera(conn, headers, res)
+        if options.camera:
+            add_camera(conn, headers, res)
         json.dump(res, sensor_js, indent=2)
         print(";", file=sensor_js)
 

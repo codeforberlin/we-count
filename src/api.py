bd756dfbfd9f43c62d78103547d9633191cb752a
Michael Behrisch <oss@behrisch.de>
2024-01-06 13:23:50 +0100

    first api call

--- src/api.py
+++ src/api.py
@@ -1,8 +1,12 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, send_file
 
 json_api = Blueprint('json_api', __name__)
 
-@json_api.route('/api/<path:path>')
-def test(path):
+@json_api.route('/api/test')
+def test():
     return jsonify({'name': 'alice',
                     'email': 'alice@bob.com'})
+
+@json_api.route('/api/v1/reports/traffic_snapshot_live')
+def traffic_snapshot_live():
+    return send_file('assets/segments.geojson', mimetype="application/json")

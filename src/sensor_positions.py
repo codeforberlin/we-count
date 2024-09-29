2f83e282f044365679c7196c70015a8c308ee67b
Michael Behrisch <oss@behrisch.de>
2024-09-29 10:35:39 +0200

    better handling of invalid input

--- src/sensor_positions.py
+++ src/sensor_positions.py
@@ -86,7 +86,7 @@ def main(args=None):
     tf = timezonefinder.TimezoneFinder()
     for segment_id in bbox_segments:
         segment_data = conns.request("/v1/segments/id/%s" % segment_id, required="features")
-        if 'features' not in segment_data:
+        if not segment_data.get('features'):
             continue
         segment = segment_data["features"][0]
         wgs_coords = segment["geometry"]["coordinates"][0]

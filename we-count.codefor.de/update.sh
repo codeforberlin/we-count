94d71d366b41d71d66962e1617d0e4ab75c19bbc
Michael Behrisch <oss@behrisch.de>
2025-07-21 03:37:43 +0200

    disable automatic pull

--- we-count.codefor.de/update.sh
+++ we-count.codefor.de/update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/we-count/we-count
-git pull
+#git pull -q
 ../venv_wecount/bin/python src/bzm_get_data.py -o "traffic_df_%s.parquet"
 /usr/bin/pkill -HUP -F /run/gunicorn/we-count/pid

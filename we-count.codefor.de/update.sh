611d2d11ff1b4daa21128786131e8e7f2c63092a
Michael Behrisch <oss@behrisch.de>
2025-07-21 03:11:15 +0200

    moving update script to parquet

--- we-count.codefor.de/update.sh
+++ we-count.codefor.de/update.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-systemctl stop we-count
-
-sudo -u we-count \
-     /home/we-count/venv_wecount/bin/python \
-     /home/we-count/we-count/src/bzm_get_data_stand_alone_v01.py
-
-systemctl start we-count
+cd /home/we-count/we-count
+git pull
+../venv_wecount/bin/python src/bzm_get_data.py -o "traffic_df_%s.parquet"
+/usr/bin/pkill -HUP -F /run/gunicorn/we-count/pid

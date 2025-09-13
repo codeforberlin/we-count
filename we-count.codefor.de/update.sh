8a3ad9016617495f0c05e06aa49bde1c89586c83
Michael Behrisch <oss@behrisch.de>
2025-09-13 13:41:34 +0200

    adding remote update

--- we-count.codefor.de/update.sh
+++ we-count.codefor.de/update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+ssh radtk.de@ssh.strato.de wecount/cgi-bin/update.py
 cd /home/we-count/we-count
 #git pull -q
-../venv_wecount/bin/python src/bzm_get_data.py -o "traffic_df_%s.parquet"
+../venv_wecount/bin/python src/bzm_get_data.py
 /usr/bin/pkill -HUP -F /run/gunicorn/we-count/pid

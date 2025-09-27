e8f32de5e8432de4b04c3214260747648bf3bdb3
Michael Behrisch <oss@behrisch.de>
2025-09-27 08:00:10 +0200

    new public version

--- we-count.codefor.de/update.sh
+++ we-count.codefor.de/update.sh
@@ -4,7 +4,7 @@ ssh radtk.de@ssh.strato.de wecount/cgi-bin/update.py
 cd $HOME/we-count
 git fetch -q
 # to make a new release update the tag / commit hash below
-git checkout 05bee44c7d68943fd3e8426001eb44bbc3bdc884
+git checkout 4ec368ed488a9e68f077db571c46758253147246
 ../venv_wecount/bin/python src/bzm_get_data.py
 /usr/bin/pkill -HUP -F /run/gunicorn/we-count/pid
 

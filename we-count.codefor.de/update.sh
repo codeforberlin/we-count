d046061a4e34e1b29b1fdf4bff34c64d3933f010
elklaassen <egbert.l.klaassen@gmail.com>
2026-02-26 17:45:04 +0100

    new public version

--- we-count.codefor.de/update.sh
+++ we-count.codefor.de/update.sh
@@ -4,7 +4,7 @@ ssh radtk.de@ssh.strato.de wecount/cgi-bin/update.py
 cd $HOME/we-count
 git fetch -q
 # to make a new release update the tag / commit hash below
-git checkout 1566f64fdebe444440509d3d797fa9b3e25a58af
+git checkout 108dd6c3f589e1965d4c6e65eff84849113bef9a
 ../venv_wecount/bin/python src/we_count/backend/bzm_get_data.py
 /usr/bin/pkill -HUP -F /run/gunicorn/we-count/pid
 

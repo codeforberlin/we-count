2a745b5ce47b3dab8c2ebec07298606319d1b85b
Michael Behrisch <oss@behrisch.de>
2026-03-03 20:43:31 +0100

    more tweaks to the update scripts

--- scripts/update_data.sh
+++ scripts/update_data.sh
@@ -15,4 +15,4 @@
 
 cd $(dirname $0)/..
 . ../venv_wecount/bin/activate
-src/we_count/backend/telraam_backup.py -j csv/bzm_telraam_segments.geojson -p parquet/bzm_telraam_traffic_data.parquet -v --csv csv/bzm_telraam --single-line-output csv/kibana/bzm_telraam_segments.geojson
+src/we_count/backend/telraam_backup.py -j csv/bzm_telraam_segments.geojson --single-line-output csv/kibana/bzm_telraam_segments.geojson -p parquet/bzm_telraam_traffic_data.parquet -v --csv csv/bzm_telraam

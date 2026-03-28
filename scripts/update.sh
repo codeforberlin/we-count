9296d58999c3f11aab63f347585771a87978a853
Michael Behrisch <oss@behrisch.de>
2026-03-28 19:01:43 +0100

    new release

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,7 +5,7 @@ cd $HOME/we-count
 # update local code
 git fetch -q
 # to make a new release update the tag / commit hash below
-git checkout cd6a3a6c46ddacb79aee607c8ca519e202632043
+git checkout 19a97d121c3cea78a3bf7a75e6f012ca78db9517
 
 # update local data and restart
 scripts/update_data.sh /srv/www/

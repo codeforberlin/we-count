1d7a0f33cc299f062caa0e194ea84791569144a2
elklaassen <egbert.l.klaassen@gmail.com>
2026-04-16 14:45:13 +0200

    new public version

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,7 +5,7 @@ cd $HOME/we-count
 # update local code
 git fetch -q
 # to make a new release update the tag / commit hash below
-git checkout 07f4b0f1e46528c248a316e6263fd37574337aab
+git checkout 602c21934b77a013fffa1fba8d26b0c6f144d165
 
 # update local data and restart
 scripts/update_data.sh /srv/www/
